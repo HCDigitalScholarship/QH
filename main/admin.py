6711ab7cb4aea8ab5f7baa0500e7388fb3cb2b93
ajanco <apjanco@gmail.com>
2020-07-08 13:25:42 -0400

    admin

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from main.models import Item
+from main.models import Item, Author, Category
 # Register your models here.
 class ItemAdmin(admin.ModelAdmin):
     search_fields = ['name', ]
 
 
 admin.site.register(Item, ItemAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name', ]
+
+
+admin.site.register(Category, CategoryAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['name', ]
+
+
+admin.site.register(Author, AuthorAdmin)
